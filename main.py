c0a585d85737278d3959796495343f30dd034a15
Axel Perez <axel-525@hotmail.com>
2023-11-06 11:37:55 -0600

    Update main.py Se agrego los botones para los filtros y se definieron los métodos para los filtros.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import numpy as np
 import cv2
 from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel, \
-    QToolBar, QMenuBar, QFileDialog
+    QToolBar, QMenuBar, QFileDialog, QVBoxLayout, QWidget, QPushButton
 from PyQt6.QtCore import Qt
 from PyQt6.QtGui import QIcon, QKeySequence, QAction, QPixmap
 
@@ -10,11 +10,11 @@ class VentanaPrincipal(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Mi ventana")
-        etiqueta = QLabel("Otro texto")
-        etiqueta.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        self.setCentralWidget(etiqueta)
+        # etiqueta = QLabel("Otro texto")
+        # etiqueta.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        # self.setCentralWidget(etiqueta)
 
-        boton_abrir = QAction(QIcon('animal.png'), "Abrir", self)
+        boton_abrir = QAction(QIcon('images/animal.png'), "Abrir", self)
         boton_abrir.setToolTip("Abrir archivo")
         boton_abrir.triggered.connect(self.botonAbrirClic)
         #Crear atajo de teclado
@@ -29,41 +29,80 @@ class VentanaPrincipal(QMainWindow):
         menu_archivo = barra_menu.addMenu('&Archivo')
         menu_archivo.addAction(boton_abrir)
 
+        layout = QVBoxLayout()
+        contenedor = QWidget()
+        contenedor.setLayout(layout)
+        self.setCentralWidget(contenedor)
+
+        boton_x = QPushButton("Filtro X")
+        boton_x.clicked.connect(self.botonFiltroX)
+        boton_y = QPushButton("Filtro Y")
+        boton_y.clicked.connect(self.botonFiltroY)
+
+        layout.addWidget(boton_x)
+        layout.addWidget(boton_y)
         
         self.pixmap = QPixmap()
         self.setMenuBar(barra_menu)
 
     def botonAbrirClic(self, s):
         fname = QFileDialog.getOpenFileName(self, 'Open file')
-        file_name = fname[0]
-        # print(fname, file_name)
+        self.file_name = fname[0]
+        print(fname, self.file_name)
 
         #cuando se seleccione una imagen
-        if file_name:
-            img = cv2.imread(file_name)
+        if self.file_name:
+            img = cv2.imread(self.file_name)
 
+            
+            # sheared_img = cv2.warpPerspective(img, M, (int(cols*1.5), int(rows*1.5)))
+
+            # cv2.imshow('original', img)
+            # cv2.imshow('sesgada',  sheared_img)
+            # cv2.waitKey(0)
+
+            # #poner la Imagen?
+            # label = QLabel(self)
+            # self.pixmap = QPixmap(self.file_name)
+            # label.setPixmap(self.pixmap)
+            # self.setCentralWidget(label)
+
+    def botonFiltroX(self):
+        if self.file_name:
+            img = cv2.imread(self.file_name)
             rows, cols, dim = img.shape
+            
             # transformation matrix for shearing
             # shearing applied to x-axis
-            M = np.float32([[1, 1.5, 0],
-             	            [2, 1  , 0],
-            	            [0, 0  , 1]])
-            # shearing applied to y-axis
-            # M = np.float32([[1,   0, 0],
-            #             	  [0.5, 1, 0],
-            #             	  [0,   0, 1]])
 
+            M = np.float32([[1, 0.5, 0],
+                            [0, 1  , 0],
+                            [0, 0  , 1]])
+            
             sheared_img = cv2.warpPerspective(img, M, (int(cols*1.5), int(rows*1.5)))
 
             cv2.imshow('original', img)
             cv2.imshow('sesgada',  sheared_img)
             cv2.waitKey(0)
 
-            # #poner la Imagen?
-            # label = QLabel(self)
-            # self.pixmap = QPixmap(file_name)
-            # label.setPixmap(self.pixmap)
-            # self.setCentralWidget(label)
+    def botonFiltroY(self):
+        if self.file_name:
+            img = cv2.imread(self.file_name)
+            rows, cols, dim = img.shape
+            
+            # transformation matrix for shearing
+            # shearing applied to x-axis
+
+            M = np.float32([[1  , 0, 0],
+                            [0.5, 1, 0],
+                            [0  , 0, 1]])
+            
+            sheared_img = cv2.warpPerspective(img, M, (int(cols*1.5), int(rows*1.5)))
+
+            cv2.imshow('original', img)
+            cv2.imshow('sesgada',  sheared_img)
+            cv2.waitKey(0)
+             
 
 app = QApplication([])
 ventana = VentanaPrincipal()
